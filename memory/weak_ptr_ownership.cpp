ad491cd14be096ea391bbd71b318825db385d542
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-07-11 23:38:20 +0300

    weak_ptr ownership example

--- memory/weak_ptr_ownership.cpp
+++ memory/weak_ptr_ownership.cpp
@@ -10,30 +10,51 @@ using std::weak_ptr;
 
 class A {
 public:
-    A() = default;
+    A(int value) : m_value(value) {}
+    int m_value;
 };
 
 class B {
 public:
     B() = default;
 
-    void action() {
-        if (pA.expired()) {
-            cout << "expired" << endl;
-        }
+    void action1() {
+        if (pA.expired())
+            cout << "B::action1() - expired" << endl;
+        else
+            pA.lock()->m_value -= 1;
     }
 
+    void action2() {
+        if (pA.expired()) {
+            cout << "B::action2() - expired" << endl;
+        }
+        else
+            cout << "Calculated value = " << pA.lock()->m_value << endl;
+    }
+
+    // B doesn't need ownership of A,
+    // it's just should know how to locate an object if it's still in memory,
+    // hence we use weak_ptr instead of shared_ptr
+    // in other words - B doesn't control lifetime
+    // of A (it's controlled by something else that B is not aware of) but sometimes B needs to access it
     weak_ptr<A> pA;
 };
 
+
+// C does have control over A's lifetime via shared_ptr
 class C {
 public:
     void foo(B& b) {
-        shared_ptr<A> pA(new A());
+        shared_ptr<A> pA(new A(2));
         b.pA = pA;
-        pA.reset(new A());
 
-        b.action();
+        b.action1();
+
+        if (pA->m_value == 0)
+            pA.reset(new A(1));
+
+        b.action2();
     }
 };
 
