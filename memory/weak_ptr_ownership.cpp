5ca311d874b2c702d345d8c1dc972e721cf06fd4
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-07-12 09:22:49 +0300

    editing comments

--- memory/weak_ptr_ownership.cpp
+++ memory/weak_ptr_ownership.cpp
@@ -36,8 +36,8 @@ public:
     // B doesn't need ownership of A,
     // it's just should know how to locate an object if it's still in memory,
     // hence we use weak_ptr instead of shared_ptr
-    // in other words - B doesn't control lifetime
-    // of A (it's controlled by something else that B is not aware of) but sometimes B needs to access it
+    // in other words - B doesn't control lifetime of A
+    // it's controlled by something else that B is not aware of, but sometimes B needs to access A
     weak_ptr<A> pA;
 };
 
@@ -61,15 +61,9 @@ public:
 
 
 int main() {
-
-    // todo: если у нас уже есть объект,
-    //  который приходит по обычному указателю, как свести его к smart ptr ?
-
     B b;
     C c;
-
     c.foo(b);
-
 
     return 0;
 }
