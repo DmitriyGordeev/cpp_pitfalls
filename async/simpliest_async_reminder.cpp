0d1bbc1049b5cdf3e309bd5b66505971aaf7cf30
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-08-15 14:50:57 +0300

    simpliest async example reminder

---
+++ async/simpliest_async_reminder.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <future>
+#include <chrono>
+
+// TODO: сделать флаг на Linux/Windows:
+#include <processthreadsapi.h>
+
+using std::cout;
+using std::endl;
+
+
+int foo() {
+    cout << "foo start" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    cout << "foo ends" << endl;
+
+    cout << "Async thread id = " << GetCurrentThreadId() << endl;
+
+    return 20;
+}
+
+int main() {
+
+
+    std::future<int> f = std::async(std::launch::async, foo);
+
+    // TODO: есть ли более простой способ ждать future ?
+    while(f.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+        cout << "main() continues | main thread id = " << GetCurrentThreadId() << endl;
+    }
+
+    cout << "result of future = " << f.get() << endl;
+
+    return 0;
+}
